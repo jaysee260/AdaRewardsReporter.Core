eef85da69b222a33d119f7cf26480d04f8c9151d
JC Gonzalez <programmer1718@gmail.com>
2021-05-30 12:05:51 -0400

    add LatestEpoch model

---
+++ Models/LatestEpoch.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace AdaRewardsReporter.Core.Models
+{
+    public class LatestEpoch
+    {
+        public int Epoch { get; set; }
+        [JsonProperty("start_time")]
+        public int StartTime { get; set; }
+        [JsonProperty("end_time")]
+        public int EndTime { get; set; }
+        [JsonProperty("first_block_time")]
+        public int FirstBlockTime { get; set; }
+        [JsonProperty("last_block_time")]
+        public int LastBlockTime { get; set; }
+        [JsonProperty("block_count")]
+        public int BlockCount { get; set; }
+        [JsonProperty("tx_count")]
+        public int TxCount { get; set; }
+        public string Output { get; set; }
+        public string Fees { get; set; }
+        [JsonProperty("active_stake")]
+        public string ActiveStake { get; set; }
+    }
+}
