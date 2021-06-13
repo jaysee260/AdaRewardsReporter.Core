7e111084c8370b06d93a4bbe19dd04ffb90d9276
JC Gonzalez <programmer1718@gmail.com>
2021-06-13 12:51:44 -0400

    strip LatestEpoch model of superflous properties

--- Models/LatestEpoch.cs
+++ Models/LatestEpoch.cs
@@ -9,17 +9,5 @@ namespace AdaRewardsReporter.Core.Models
         public int StartTime { get; set; }
         [JsonProperty("end_time")]
         public int EndTime { get; set; }
-        [JsonProperty("first_block_time")]
-        public int FirstBlockTime { get; set; }
-        [JsonProperty("last_block_time")]
-        public int LastBlockTime { get; set; }
-        [JsonProperty("block_count")]
-        public int BlockCount { get; set; }
-        [JsonProperty("tx_count")]
-        public int TxCount { get; set; }
-        public string Output { get; set; }
-        public string Fees { get; set; }
-        [JsonProperty("active_stake")]
-        public string ActiveStake { get; set; }
     }
 }
